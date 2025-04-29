3833262b98620caa8dda46e547c42e3aecb32d1b
Rukshan-Vimukthi <rukshanse.info@gmail.com>
2025-04-29 16:59:12 +0530

    Fixed path to work on github pages

--- frontend/src/User/Components/Header.tsx
+++ frontend/src/User/Components/Header.tsx
@@ -9,15 +9,15 @@ export default function Header(){
                 </NavbarBrand>
                 <NavbarCollapse>
                     <Nav className="d-flex flex-column flex-md-row gap-md-4 gap-0">
-                        <NavLink href="/" style={{color: "white"}}>Home</NavLink>
-                        <NavLink href="/about" style={{color: "white"}}>About Us</NavLink>
-                        <NavLink href="/contact-us" style={{color: "white"}}>Contact Us</NavLink>
+                        <NavLink href="/restaurant-web-application/" style={{color: "white"}}>Home</NavLink>
+                        <NavLink href="/restaurant-web-application/about" style={{color: "white"}}>About Us</NavLink>
+                        <NavLink href="/restaurant-web-application/contact-us" style={{color: "white"}}>Contact Us</NavLink>
                     </Nav>
                 </NavbarCollapse>
             </Container>
             <Container className="justify-content-end pe-4 gap-3">
-                <NavLink href="/register" style={{color: "white"}} className=" align-self-end w-auto">Register</NavLink>
-                <NavLink href="/login" style={{color: "white"}} className=" align-self-end w-auto">Login</NavLink>
+                <NavLink href="/restaurant-web-application/register" style={{color: "white"}} className=" align-self-end w-auto">Register</NavLink>
+                <NavLink href="/restaurant-web-application/login" style={{color: "white"}} className=" align-self-end w-auto">Login</NavLink>
             </Container>
         </Navbar>
     );
